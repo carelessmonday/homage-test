586061f4752867e84060be3e28627d3887134ad7
Kevin James <kevinjames@protonmail.ch>
2021-11-26 17:59:24 +0800

    lint fix

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
   client: 'sqlite3',
   connection: {
-    filename: './data/database.db3'
+    filename: './data/database.db3',
   },
   useNullAsDefault: true,
   migrations: {
-    directory: './data/migrations'
+    directory: './data/migrations',
   },
   seeds: {
-    directory: './data/seeds'
-  }
+    directory: './data/seeds',
+  },
 };
